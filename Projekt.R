91bb300692530c7434936dad22abde1326c5afe2
Michal-Predota <80167183+Michal-Predota@users.noreply.github.com>
2023-12-17 12:41:35 +0100

    Dodane tytuły, napoczęta funkcja od POS
    Nie wiem czy coś źle patrzę ale chyba nie działa nam dobrze pobieranie? Książki Shakespeara się nie pobierają, nie wiem czy coś mamy źle czy to R odwala

--- Projekt.R
+++ Projekt.R
@@ -5,13 +5,41 @@ library(tidytext)
 library(dplyr)
 library(magrittr)
 library(ggplot2)
+
+
+
+pos <- parts_of_speech %>%
+  count(pos, sort = T) %>%
+  mutate(nn = n / sum(n))
+
+
+
+
+pos_freq<-function(book)
+{
+    f<-book%>%left_join(pos) %>%
+    count(pos, sort = T) %>%
+    mutate(nn = n / sum(n))
+  
+  return(f)
+}
+
+
+
+
+
 Shak_id <- c(2245, 2239, 1777, 2243, 2242, 1785, 1781, 2265, 2267, 1791, 1794, 1795, 2268, 1797)
 Shak <- gutenberg_download(Shak_id)
 
+Shak_tit<-c("The Comedie of Errors", "Romeo and Juliet", "The Merchant of Venice", "A Midsummer Night's Dream", "The Trgaedy of julius Caesar", "The Merry Wives of Windsor", "The Tragedie of Hamlet", "Othello", "Alls Well That Ends Well", "The Tragedy of King Lear", "The Tragedy of Macbeth", "The Tragedie of Anthonie and Cleopathra", "The Tragedy of Coriolanius")
+
 Aust <- gutenberg_download(c(1212, 946, 42671, 141, 158))
+
+Aust_tit<-c("Love and Freindship", "Lady Susan", "Pride and Prejudice", "Mansfield Park", "Emma")
 
 Dick <- gutenberg_download(c(675, 19337, 653, 678, 676, 821, 644, 42232, 699, 786, 1422, 1415, 809, 1394))
 
+Dick_tit<-c("American Notes for General Circulation", "A Christmas Carol", "The Chimes", "The Cricket on The Harth A Fairy Tale of Home", "The Battle of Life", "Dombey and Son", "The Haunted Man and the Ghost's Bargain", "A Child's Dream of a Star", "A Child's History of England", "Hard Times", "Going into Society", "Doctor Marigold", "Holiday Romance in Four Parts", "The Holly-Tree")
 
 g <- gutenberg_works()
 
@@ -41,7 +69,34 @@ ggplot(Shak_book1) +
   labs(y = "fraction", x = "POS")
 
 
+
+Shak
+
+
+
+
+
+
+
+
+
 #Przyszłość
-#Zapisać tytuły w wektorze, chronologicznie
-#Zrobić funkcję która będzie wypluwała danej części mowy (argument: lista książek, zwraca: df)
+#Zapisać tytuły w wektorze, chronologicznie - zrobione
+#Zrobić funkcję która będzie wypluwała częstość danej części mowy (argument: lista książek, zwraca: df)
 #Napisać funkcję, która będzie robiła przebiegi dla konkretnej części mowy dla kilku autorów
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
